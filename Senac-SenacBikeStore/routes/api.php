a4eca0f3999e2a4c1a57e54b4295bc431ca53434
LUCCA PRESTES BORDIN <lucca.pbordin@senacsp.edu.br>
2023-04-10 22:30:13 -0300

    Subindo novo filtro com tratamento

--- Senac-SenacBikeStore/routes/api.php
+++ Senac-SenacBikeStore/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\MarcaController;
+use App\Http\Controllers\Api\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +26,8 @@ route::apiResource('categorias', CategoriaController::class);
 
 //Marcas
 route::apiResource('marcas', MarcaController::class);
+
+//Produtos
+route::apiResource('produtos', ProdutoController::class);
 
 
